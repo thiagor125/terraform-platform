c8638cc20baf63c77c6327547c226b76da0b782d
thiago rodrigues oliveira <95186623+thiagor125@users.noreply.github.com>
2026-09-05 01:36:51 -0300

    fix: make AWS lab outputs lifecycle-safe

--- environments/aws/lab/outputs.tf
+++ environments/aws/lab/outputs.tf
@@ -1,24 +1,24 @@
 output "vpc_id" {
-  description = "ID of the lab VPC."
-  value       = module.network.vpc_id
+  description = "ID of the lab VPC when the network is deployed."
+  value       = var.deploy_network ? module.network[0].vpc_id : null
 }
 
 output "vpc_cidr" {
-  description = "CIDR block of the lab VPC."
-  value       = module.network.vpc_cidr
+  description = "CIDR block of the lab VPC when the network is deployed."
+  value       = var.deploy_network ? module.network[0].vpc_cidr : null
 }
 
 output "public_subnet_ids" {
-  description = "IDs of the lab public subnets."
-  value       = module.network.public_subnet_ids
+  description = "IDs of the lab public subnets when the network is deployed."
+  value       = var.deploy_network ? module.network[0].public_subnet_ids : []
 }
 
 output "private_subnet_ids" {
-  description = "IDs of the lab private subnets."
-  value       = module.network.private_subnet_ids
+  description = "IDs of the lab private subnets when the network is deployed."
+  value       = var.deploy_network ? module.network[0].private_subnet_ids : []
 }
 
 output "internet_gateway_id" {
-  description = "ID of the Internet Gateway."
-  value       = module.network.internet_gateway_id
+  description = "ID of the Internet Gateway when the network is deployed."
+  value       = var.deploy_network ? module.network[0].internet_gateway_id : null
 }
